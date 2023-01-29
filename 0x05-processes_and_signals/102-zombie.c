7f9b70108cf7a15bc4c84ed0bae62e83c5c3d75a
Maryswy <marysowa54@gmail.com>
2023-01-29 15:05:02 -0800

    a C program that creates 5 zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * infinite_while - while loop
+ * Return - Always Return 0 if successful
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+int main(void)
+{
+	int i = 0;
+	pid_t child_p = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_p = fork();
+
+		if (child_p > 0)
+			printf("Zombie process created, PID: %d\n", child_p);
+		else
+			exit(0);
+	}
+
+	return (infinite_while());
+
+}
