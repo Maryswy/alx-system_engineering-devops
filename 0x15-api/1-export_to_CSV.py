6a119229a3d093c9b4bd174fca9eedac758198d1
Maryswy <marysowa54@gmail.com>
2023-04-25 02:38:37 +0300

    extend your Python script to export data in the CSV format (Export to CSV)

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python3
 """
-This module provides a function to export task data to CSV format
+Using what you did in the task #0, extend your Python script to export data in the CSV format
 """
-
 import csv
 import requests
+from sys import argv
 
-# Define function to export data to CSV format
+
+def to_csv():
+    """return API data"""
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for u in users.json():
+        if u.get('id') == int(argv[1]):
+            USERNAME = (u.get('username'))
+            break
+    TASK_STATUS_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
 
 
-def export_tasks_to_csv(user_id, username, tasks):
-    # Set up file name based on user_id
-    file_name = f"{user_id}.csv"
-
-    # Open file in write mode and write headers
-    with open(file_name, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-
-        # Loop through tasks and write each row to the CSV file
-        for task in tasks:
-            writer.writerow([user_id, username, task["completed"],
-                             task["title"]])
-
-        # Print message indicating successful export
-        print(f"{len(tasks)} tasks exported {file_name} for user {user_id})")
+    """export to csv"""
+    filename = "{}.csv".format(argv[1])
+    with open(filename, "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in TASK_STATUS_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                             "TASK_COMPLETED_STATUS": task[0],
+                             "TASK_TITLE": task[1]})
 
 
 if __name__ == "__main__":
-    # Example usage
-    user_id = 2
-    username = "Antonette"
-    tasks = [
-        {"userId": 2, "title": "suscipit repellat ", "completed": False},
-        {"userId": 2, "title": "distinctio vitae aute", "completed": True},
-        {"userId": 2, "title": "et itaque necessit", "completed": False},
-        {"userId": 2, "title": "adipisci non ad ", "completed": False},
-        {"userId": 2, "title": "voluptas quo ", "completed": True},
-        {"userId": 2, "title": "aliquam aut quasi", "completed": True},
-        {"userId": 2, "title": "veritatis pariatur ", "completed": True},
-        {"userId": 2, "title": "nesciunt totam sit", "completed": False},
-        {"userId": 2, "title": "laborum aut in quam", "completed": False},
-        {"userId": 2, "title": "nemo perspiciatis repell", "completed": True},
-        {"userId": 2, "title": "repudiandae totam in es", "completed": False},
-        {"userId": 2, "title": "earum doloribus ea dolo", "completed": False},
-        {"userId": 2, "title": "sint sit aut vero", "completed": False},
-        {"userId": 2, "title": "porro aut necess", "completed": False},
-        {"userId": 2, "title": "repellendus veritatis ", "completed": True},
-        {"userId": 2, "title": "excepturi deleniti ", "completed": True},
-        {"userId": 2, "title": "sunt cum tempora", "completed": False},
-        {"userId": 2, "title": "totam quia non", "completed": False},
-        {"userId": 2, "title": "doloremque quibusdam as", "completed": False},
-        {"userId": 2, "title": "totam atque quo nesciunt", "completed": True},
-    ]
+    to_csv()
 
-    export_tasks_to_csv(user_id, username, tasks)
