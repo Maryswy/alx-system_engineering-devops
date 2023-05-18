0bf952069a734c7a7ae2e7ccbd0ff92a6ba04eb0
Maryswy <marysowa54@gmail.com>
2023-05-18 23:34:11 +0300

    Sky is the limit web debugging 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,7 @@
-# HTTP requests amount fix
+# Fix Nginx
 
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
-  }
 
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  }
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
